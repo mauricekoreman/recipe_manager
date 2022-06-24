841291444a3bd256669bf08e75080784c4528dde
mauricekoreman <mg.koreman99@gmail.com>
2022-06-24 17:13:50 +0200

    prepare deploy

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 // const morgan = require('morgan'); // TODO: find out what this is?
 
 const v1Router = require("./routes/v1");
@@ -13,6 +14,15 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/v1", v1Router);
 
-app.get("/", (req, res) => res.send("hello there, Maurice"));
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../", "../", "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 module.exports = app;
